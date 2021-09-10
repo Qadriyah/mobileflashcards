e7d71c802fd003f9d86c3397eb7fbefe2bf697b8
Baker Sekitoleko <b.alzawad@gmail.com>
2021-09-10 20:58:58 +0300

    feat: Create add new deck form
    - Creates add new deck form
    - Creates a loader component
    - Creates a button component

--- src/screens/NewDeckScreen/NewDeckScreen.js
+++ src/screens/NewDeckScreen/NewDeckScreen.js
@@ -1,20 +1,47 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { useDispatch } from "react-redux";
+import { View, Text, TextInput } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 import styles from "./styles";
 import { saveDeckTitle } from "../../redux/actions/deck";
+import Colors from "../../utils/colors";
+import Button from "../../components/Button/Button";
+import Loader from "../../components/Loader/Loader";
 
-const NewDeckScreen = () => {
+const NewDeckScreen = ({ navigation }) => {
   const dispatch = useDispatch();
+  const [title, setTitle] = React.useState("");
+  const [error, setError] = React.useState("");
+  const { requesting } = useSelector((state) => state.decks);
 
-  React.useEffect(() => {
-    // dispatch(saveDeckTitle("Diamond"));
-  }, [dispatch]);
+  const onChangeText = (text) => {
+    setError("");
+    setTitle(text);
+  };
+
+  const onSubmit = () => {
+    if (!title) {
+      setError("Title is required.");
+      return;
+    }
+    dispatch(saveDeckTitle(title)).then(() => {
+      setTitle("");
+      navigation.navigate("Decks");
+    });
+  };
 
   return (
-    <View>
-      <Text>New deck</Text>
+    <View style={styles.container}>
+      {requesting && <Loader loading={requesting} color={Colors.WHITE} />}
+      <Text style={styles.label}>Enter the title of your new deck</Text>
+      <TextInput
+        style={[styles.input, error ? { shadowColor: Colors.RED } : null]}
+        value={title}
+        placeholder="Deck Title"
+        placeholderTextColor={Colors.GREY}
+        onChangeText={(text) => onChangeText(text)}
+      />
+      <Button label="Submit" onPress={onSubmit} />
     </View>
   );
 };
